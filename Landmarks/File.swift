f55b941f04d00338d8c576f307de2570a113da73
Gan Tu <github@tugan.dev>
2021-06-28 17:44:35 -0700

    add tutorials section 1 and 2 badges

---
+++ Landmarks/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Landmarks
+//
+//  Created by Gan Tu on 6/27/21.
+//
+
+import Foundation
---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,48 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Gan Tu on 6/27/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData : ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(
+                        destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {    
+    static var previews: some View {
+        ForEach(["iPhone XS Max"], id: \.self) { deviceName in
+           LandmarkList()
+                .environmentObject(ModelData())
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
