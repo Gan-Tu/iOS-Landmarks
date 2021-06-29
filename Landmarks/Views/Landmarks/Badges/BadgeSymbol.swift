50adc58f2a07744c29820b5f045ecc737ea9aaa6
Gan Tu <github@tugan.dev>
2021-06-28 18:39:35 -0700

    add comments to badge drawing parameters

--- Landmarks/Views/Landmarks/Badges/BadgeSymbol.swift
+++ Landmarks/Views/Landmarks/Badges/BadgeSymbol.swift
@@ -13,12 +13,14 @@ struct BadgeSymbol: View {
     var body: some View {
         GeometryReader { geometry in
             Path { path in
+                // The total bonding box size of our badge
                 let width = min(geometry.size.width, geometry.size.height)
                 let height = width * 0.75
-                let spacing = width * 0.030
-                let middle = width * 0.5
-                let topWidth = width * 0.226
-                let topHeight = height * 0.488
+                
+                let spacing = width * 0.030     // arbitrary spacing constant
+                let middle = width * 0.5        // the x axis for the middle line
+                let topWidth = width * 0.226    // length from middle to side for top triangle
+                let topHeight = height * 0.488  // height to mid point of left/right white strip
                 
                 path.addLines([
                     CGPoint(x: middle, y: spacing),
